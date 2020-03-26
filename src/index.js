5c735dc692d8a4091791a2e0cd43a6cb773e6c58
Amish Shah <amishshah.2k@gmail.com>
2020-03-26 18:20:06 +0000

    feat: start work on client

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('Hello world!');
+const { AkairoClient, CommandHandler } = require('discord-akairo');
+const config = require('../data/config.json');
+
+class Client extends AkairoClient {
+	constructor() {
+		super({
+			ownerID: config.discord.owners
+		});
+
+		this.commandHandler = new CommandHandler(this, {
+			blockBots: true,
+			blockClient: true,
+			prefix: config.discord.prefix,
+			allowMention: true,
+			directory: './src/commands/'
+		});
+
+		this.commandHandler.loadAll();
+	}
+}
+
+const client = new Client();
+client.login(config.discord.bot_token);
