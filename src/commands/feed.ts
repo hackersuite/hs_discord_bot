7ae492c8f9a96dda30d1078afe47ac94c7d032a1
Amish Shah <amishshah.2k@gmail.com>
2020-11-27 14:26:35 +0000

    feat: feeding users (#23)
    * feat: groundwork for feed command
    
    * feat: hs_hub integration

---
+++ src/commands/feed.ts
@@ -0,0 +1,70 @@
+import { Message, TextChannel, DMChannel, GuildMember } from 'discord.js';
+import { Command } from 'discord-akairo';
+import { Task, TaskStatus } from '../util/task';
+import { HackathonClient } from '../HackathonClient';
+import fetch from 'node-fetch';
+import { getUser } from '@unicsmcr/hs_discord_bot_api_client';
+
+const ACHIEVEMENT_ID: string = process.env.FEED_ACHIEVEMENT_ID!;
+if (!ACHIEVEMENT_ID) {
+	throw new Error('FEED_ACHIEVEMENT_ID must be set');
+}
+
+export default class FeedCommand extends Command {
+	public constructor() {
+		super('feed', {
+			aliases: ['feed'],
+			args: [
+				{
+					id: 'target',
+					type: 'member',
+					prompt: {
+						start: 'Who would you like to feed?',
+						retry: 'That\'s not a valid member! Try again.'
+					}
+				}
+			],
+			channel: 'guild'
+		});
+	}
+
+	public async exec(message: Message, args: { target: GuildMember }) {
+		const client = message.client as HackathonClient;
+		const task = new Task({
+			title: 'Feed Member 🍕',
+			issuer: message.author,
+			description: `Feeding **${args.target.user.tag}** (${args.target.id})`
+		});
+		await task.sendTo(message.channel as TextChannel | DMChannel);
+
+		try {
+			if (!(await client.discordUserCanAccessResource(message.author.id, 'hs:hs_discord:bot:feed'))) {
+				return task.update({
+					status: TaskStatus.Failed,
+					description: 'Sorry, you do not have permission to feed the participants :('
+				});
+			}
+
+			const authUser = await getUser(args.target.id);
+
+			await fetch(`https://hub.greatunihack.com/achievements/${ACHIEVEMENT_ID}/api/complete`, {
+				method: 'PUT',
+				body: JSON.stringify({ userId: authUser.authId }),
+				headers: {
+					Authorization: client.config.hsAuth.token
+				}
+			});
+
+			await task.update({
+				status: TaskStatus.Completed,
+				description: `Fed **${args.target.user.tag}** (${args.target.id})`
+			});
+		} catch (err) {
+			client.loggers.bot.warn(err);
+			task.update({
+				status: TaskStatus.Failed,
+				description: `An error occurred processing your request. Please try again later.`
+			}).catch(err => client.loggers.bot.warn(err));
+		}
+	}
+}
